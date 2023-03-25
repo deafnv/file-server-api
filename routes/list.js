8b0f50853104db1130e32fd03d39fe08ed1f39c4
deafnv <rokiniri@yahoo.in>
2023-03-25 21:46:44 +0800

    feat: hide root directory

--- routes/list.js
+++ routes/list.js
@@ -6,7 +6,6 @@ const router = express.Router()
 router.get('/:filename(*)', (req, res) => {
   const fileName = req.params.filename
   let queryPath = process.env.ROOT_DIRECTORY_PATH + '/' + (fileName ? fileName + '/' : '') 
-  console.log(queryPath)
   fs.readdir(queryPath, (err, files) => {
     if (err) {
       res.status(500).send(err)
@@ -25,7 +24,7 @@ router.get('/:filename(*)', (req, res) => {
           } else {
             const fileObj = {
               name: file,
-              path: filePath,
+              path: filePath.replace(process.env.ROOT_DIRECTORY_PATH, ''),
               size: stats.size,
               created: stats.birthtime,
               modified: stats.mtime,
