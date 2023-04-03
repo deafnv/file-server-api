36f9621ab8bc162b436c332b34b6885d287e1f87
deafnv <rokiniri@yahoo.in>
2023-04-04 03:44:16 +0800

    feat: omit path from diskspace

--- routes/diskspace.js
+++ routes/diskspace.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import checkDiskSpace from 'check-disk-space'
+import omit from 'lodash/omit.js'
 
 const router = express.Router()
 
 router.get('/', async (req, res) => {
   checkDiskSpace(process.env.ROOT_DIRECTORY_PATH)
     .then((diskSpace) => {
-      console.log(diskSpace)
-      res.status(200).send(diskSpace)
+      res.status(200).send(omit(diskSpace, 'diskPath'))
     })
     .catch((err) => {
       console.log(err)
