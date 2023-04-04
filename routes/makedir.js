05e21dd8c1f70b6419fe2178b40705c1ef55530b
deafnv <rokiniri@yahoo.in>
2023-04-05 03:37:06 +0800

    refactor: better makedir route

--- routes/makedir.js
+++ routes/makedir.js
@@ -1,14 +1,15 @@
 import express from 'express'
 import fs from 'fs'
+import path from 'path'
 import authorize from '../lib/authorize-func.js'
 
 const router = express.Router()
 
 router.post('/', authorize, (req, res) => {
-  const { dirname, path } = req.body
-  if (typeof dirname != 'string' || typeof path != 'string' || !dirname || !path)
+  const { newDirName, currentPath } = req.body
+  if (typeof newDirName != 'string' || typeof currentPath != 'string' || !newDirName || !currentPath)
     return res.status(400).send('Missing required body')
-  let queryPath = process.env.ROOT_DIRECTORY_PATH + path + '/' + dirname
+  let queryPath = path.join(process.env.ROOT_DIRECTORY_PATH, currentPath, newDirName)
 
   fs.mkdir(queryPath, (err) => {
     if (err) {
