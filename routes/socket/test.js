f0587d8949b40392a934c414a18cd03ff347e498
deafnv <rokiniri@yahoo.in>
2023-04-29 09:05:23 +0800

    ts initial

--- routes/socket/test.js
+++
@@ -1,8 +0,0 @@
-const test = (io, socket) => {
-  socket.on('test', (message) => {
-    console.log('Message: ' + message)
-    socket.broadcast.emit('test', message)
-  })
-}
-
-export default test
