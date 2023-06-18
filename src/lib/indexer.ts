e04d0303cfdd047409ab98366283879277a43a4b
deafnv <rokiniri@yahoo.in>
2023-06-18 13:26:25 +0800

    refactor: changed search to more like /list

--- src/lib/indexer.ts
+++ src/lib/indexer.ts
@@ -3,9 +3,8 @@ import path from 'path'
 import fs from 'fs-extra'
 import { minimatch } from 'minimatch'
 import Fuse from 'fuse.js'
-import omit from 'lodash/omit.js'
 
-import { excludedDirs, metadataEnabled, rootDirectoryPath } from './config.js'
+import { excludedDirs, rootDirectoryPath } from './config.js'
 
 export let indexArray: any[] = []
 //@ts-ignore
@@ -14,36 +13,23 @@ export const fuse = new Fuse([], { keys: ['name'] })
 export async function indexFiles(directoryPath = rootDirectoryPath) {
   await indexTraverser(directoryPath)
 
-  //indexArray.sort((a, b) => a.name.localeCompare(b.name))
   fuse.setCollection(indexArray)
-  indexArray = null
+  indexArray = []
 }
 
 async function indexTraverser(directoryPath: string) {
   const files = await fs.readdir(directoryPath)
   for (const file of files) {
     const filePath = path.join(directoryPath, file)
-    const fileStats = await fs.stat(filePath)
 
     //* Exclude directories & shortcuts
     if (excludedDirs.some((excludedDir) => minimatch(filePath, excludedDir))) continue
     if (file.includes('.shortcut.json')) continue
 
-    if (fileStats.isDirectory()) {
-      const metadata =
-        metadataEnabled && (await fs.exists(path.join(filePath, '.metadata.json')))
-          ? JSON.parse(await fs.readFile(path.join(filePath, '.metadata.json'), 'utf-8'))
-          : {}
-
+    if ((await fs.stat(filePath)).isDirectory()) {
       indexArray.push({
         name: file,
         path: filePath.replace(rootDirectoryPath, '').replaceAll(path.sep, '/'),
-        size: fileStats.size,
-        created: fileStats.birthtime,
-        modified: fileStats.mtime,
-        isDirectory: true,
-        isShortcut: null,
-        metadata: Object.keys(metadata).length ? omit(metadata, ['name', 'path']) : undefined,
       })
 
       await indexTraverser(filePath)
@@ -51,11 +37,6 @@ async function indexTraverser(directoryPath: string) {
       indexArray.push({
         name: file,
         path: filePath.replace(rootDirectoryPath, '').replaceAll(path.sep, '/'),
-        size: fileStats.size,
-        created: fileStats.birthtime,
-        modified: fileStats.mtime,
-        isDirectory: true,
-        isShortcut: null,
       })
     }
   }
--- src/routes/query/search.ts
+++ src/routes/query/search.ts
@@ -1,6 +1,15 @@
-import express, { RequestHandler } from 'express'
+import path from 'path'
 
-import { isListRequireAuth, protectedPaths } from '../../lib/config.js'
+import express, { RequestHandler } from 'express'
+import fs from 'fs-extra'
+import omit from 'lodash/omit.js'
+
+import {
+  isListRequireAuth,
+  metadataEnabled,
+  protectedPaths,
+  rootDirectoryPath,
+} from '../../lib/config.js'
 import authorize, { isRouteInArray } from '../../lib/authorize-func.js'
 import { fuse } from '../../lib/indexer.js'
 
@@ -25,7 +34,39 @@ router.get('/', authHandler, postAuthHandler, async (req, res) => {
   const searchTerm = req.query.q
   const results = fuse.search(searchTerm, { limit: 50 })
 
-  return res.status(200).send(results)
+  let resultsList = []
+
+  if (results.length) {
+    for (const result of results) {
+      const { name, path: resultPath } = result.item
+      const filePath = path.join(rootDirectoryPath, resultPath)
+
+      try {
+        const fileStats = await fs.stat(filePath)
+        const metadata =
+          fileStats.isDirectory() &&
+          metadataEnabled &&
+          (await fs.exists(path.join(filePath, '.metadata.json')))
+            ? JSON.parse(await fs.readFile(path.join(filePath, '.metadata.json'), 'utf-8'))
+            : {}
+        resultsList.push({
+          name: name,
+          path: resultPath,
+          size: fileStats.size,
+          created: fileStats.birthtime,
+          modified: fileStats.mtime,
+          isDirectory: fileStats.isDirectory(),
+          isShortcut: null,
+          metadata: Object.keys(metadata).length ? omit(metadata, ['name', 'path']) : undefined,
+        })
+      } catch (error) {
+        console.error(error)
+        return res.status(500).send(error)
+      }
+    }
+  }
+
+  return res.status(200).send(resultsList)
 })
 
 export default router
